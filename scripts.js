81fa1ffb55c83c4a004d50db2aaa29a57aae7777
iwnlMski <mski.python@gmail.com>
2021-02-07 13:58:30 +0100

    Refactor win conditions and HTML element names

--- scripts.js
+++ scripts.js
@@ -2,24 +2,25 @@
 // playerRed = prompt("Player Two is playing red chips \n Enter your name: ")
 //
 
+chipHeight = 7
+bottomChipHeight = 35
 var redChip = '🔴'
 var blueChip = '🔵'
-var redColor = 'red'
-var blueColor = 'blue'
 var currentColor = redChip
-$('#nColor').text(redChip)
+$('#helpMessageChipColorIndicator').text(redChip)
+rgbRedColor = 'rgb(255, 0, 0)'
+rgbBlueColor = 'rgb(0, 0, 255)'
+rgbNoChipColorGrey = 'rgb(128, 128, 128)'
 
 var circles = $('.circle')
 
 function addChipToLowestLevelInColumn(columnNumber, chip) {
-    noChipColorGrey = 'rgb(128, 128, 128)'
-    if (circles.eq(columnNumber).css('background-color') == noChipColorGrey) {
-        chipHeight = 7
+    if (circles.eq(columnNumber).css('background-color') == rgbNoChipColorGrey) {
         bottomChipHeight = 35
         for (var currentLevel = bottomChipHeight + columnNumber; currentLevel >= 0; currentLevel -= chipHeight) {
             bottomColumnSlot = circles.eq(currentLevel)
-            if (bottomColumnSlot.css('background-color') == noChipColorGrey) {
-                bottomColumnSlot.css('background-color', chip == redChip ? redColor : blueColor)
+            if (bottomColumnSlot.css('background-color') == rgbNoChipColorGrey) {
+                bottomColumnSlot.css('background-color', chip == redChip ? rgbRedColor : rgbBlueColor)
                 break;
             }
         }
@@ -28,52 +29,42 @@ function addChipToLowestLevelInColumn(columnNumber, chip) {
 
 function changeColor() {
     currentColor = currentColor == redChip ? blueChip : redChip
-    $('#nColor').text(currentColor)
-    $('#nPLayer').text(currentColor == redChip ? 'One' : 'Two')
+    $('#helpMessageChipColorIndicator').text(currentColor)
+    $('#textPLayerNumber').text(currentColor == redChip ? 'One' : 'Two')
 }
 
-function checkWin() {
-    //col
-    for (var col = 0; col < 7; col++) {
-        var win = 0
-        for (var index = col + 35; index >= 0; index -= 7) {
-            if (circles.eq(index).css('background-color') == 'rgb(255, 0, 0)') {
-                win++
-            } else if (circles.eq(index).css('background-color') == 'rgb(0, 0, 255)') {
-                ``
-                win--;
+function checkWinAndAlertOnWin() {
+    numberOfColumns = 7
+    var redWins = 4
+    var blueWins = -4
+    for (var column = 0; column < numberOfColumns; column++) {
+        var chipsInARow = 0
+        for (var index = column + bottomChipHeight; index >= 0; index -= chipHeight) {
+            currentChipColor = circles.eq(index).css('background-color')
+            if(currentChipColor == rgbBlueColor || currentChipColor == rgbRedColor){
+                chipsInARow += currentChipColor == rgbBlueColor ? -1 : 1
             }
-            if (win == 4) {
-                alert("Red WINS!");
-                break;
-            } else if (win == -4) {
-                alert("Blue WINS!");
+            if(chipsInARow == redWins || chipsInARow == blueWins){
+                alert(chipsInARow == redWins ? "RED WINS!" : "BLUE WINS?")
                 break;
             }
         }
     }
 
-    //row
-    for (var row = 0; row <= 35; row += 7) {
-        var win = 0;
+    for (var row = 0; row <= bottomChipHeight; row += chipHeight) {
+        var chipsInARow = 0;
         for (var index = row; index < row + 7; index++) {
-            if (circles.eq(index).css('background-color') == 'rgb(255, 0, 0)') {
-                win++;
-            } else if (circles.eq(index).css('background-color') == 'rgb(0, 0, 255)') {
-                win--;
+            currentChipColor = circles.eq(index).css('background-color')
+            if(currentChipColor == rgbBlueColor || currentChipColor == rgbRedColor) {
+                chipsInARow += currentChipColor == rgbBlueColor ? -1 : 1
             }
-            if (win == 4) {
-                alert("Red WINS!");
-                break;
-            } else if (win == -4) {
-                alert("Blue WINS!");
+            if(chipsInARow == redWins || chipsInARow == blueWins){
+                alert(chipsInARow == redWins ? "RED WINS!" : "BLUE WINS?")
                 break;
             }
         }
     }
     //diagonaltoprightsided
-
-
 }
 
 
@@ -81,6 +72,6 @@ $('.game_column').each(function (i, column) {
     $(column).click(function () {
         addChipToLowestLevelInColumn($(column).attr('id'), currentColor);
         changeColor();
-        checkWin();
+        checkWinAndAlertOnWin();
     })
 });
